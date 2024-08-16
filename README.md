279d96fcbba6bb9578069e03d77df6691787dbf9
shinich39 <shinich39@gmail.com>
2024-08-16 14:50:21 +0900

    update

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Create a new group of nodes.
 
 ## Features
 
-- Select nodes in group.
+Select nodes in group.  
+Bypass nodes in group.  
 
 ## Usage
 
 Select nodes to create a new group.  
 Press Ctrl + 0-9  
 Press 0-9 to select nodes in group.  
+Double presss 0-9 to bypass nodes in group.  
--- js/index.js
+++ js/index.js
@@ -3,16 +3,24 @@
 import { app } from "../../../scripts/app.js";
 import { api } from "../../../scripts/api.js";
 
+const PROPERTY_KEY = "__group__";
+const DOUBLE_PRESS_DELAY = 1000 * 0.3; // 0.3s
+
 function getSelectedNodes() {
   return app.graph._nodes.filter(e => e.is_selected);
 }
 
 function getGroup(n) {
-  return n?.properties?.groupSelection;
+  return n?.properties?.[PROPERTY_KEY];
+}
+
+function isGroup(node, group) {
+  const g = node?.properties?.[PROPERTY_KEY];
+  return Array.isArray(g) && g.indexOf(group) > -1;
 }
 
 function getGroupNodes(g) {
-  return app.graph._nodes.filter(e => getGroup(e) == g);
+  return app.graph._nodes.filter(n => isGroup(n, g));
 }
 
 function selectNodes(nodes) {
@@ -23,12 +31,50 @@ function deselectAll() {
   app.canvas.deselectAllNodes();
 }
 
+// bypass
+function enableNodes(nodes) {
+  for (const n of nodes) {
+    n.mode = 0;
+  }
+}
+
+function disableNodes(nodes) {
+  for (const n of nodes) {
+    n.mode = 4;
+  }
+}
+
+function isEnabledNodes(nodes) {
+  for (const n of nodes) {
+    if (n.mode === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function toggleNodes(nodes) {
+  let isEnabled = isEnabledNodes(nodes);
+  if (isEnabled) {
+    disableNodes(nodes);
+  } else {
+    enableNodes(nodes);
+  }
+}
+
 ;(() => {
   try {
+    let prevGroup = -1, 
+        prevTime = Date.now();
+
     function keydownEvent(e) {
       try {
         const { key } = e;
         const ctrlKey = e.ctrlKey || e.metaKey;
+
+        if (e.target != document.body && e.target.id != "graph-canvas") {
+          return;
+        }
 
         if (!/^[0-9]$/.test(key)) {
           return;
@@ -39,20 +85,34 @@ function deselectAll() {
           // remove group
           const prevNodes = getGroupNodes(key);
           for (const n of prevNodes) {
-            delete n.properties.groupSelection;
+            if (!n.properties[PROPERTY_KEY]) {
+              n.properties[PROPERTY_KEY] = [];
+            } else {
+              n.properties[PROPERTY_KEY] = n.properties[PROPERTY_KEY].filter(e => e != key);
+            }
           }
 
           // set group
           const currNodes = getSelectedNodes();
           for (const n of currNodes) {
-            n.properties.groupSelection = parseInt(key);
+            if (!n.properties[PROPERTY_KEY]) {
+              n.properties[PROPERTY_KEY] = [key];
+            } else {
+              n.properties[PROPERTY_KEY].push(key);
+            }
           }
         } else {
           const groupNodes = getGroupNodes(key);
           if (groupNodes.length > 0) {
             e.preventDefault();
-            deselectAll();
-            selectNodes(groupNodes);
+            if (prevGroup == key && prevTime + DOUBLE_PRESS_DELAY >= Date.now()) {
+              toggleNodes(groupNodes);
+            } else {
+              deselectAll();
+              selectNodes(groupNodes);
+              prevGroup = key;
+              prevTime = Date.now();
+            }
           }
         }
       } catch(err) {
